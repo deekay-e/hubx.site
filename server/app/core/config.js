8e675c61719cb5137d8ae5b1c3b1c27247b4c551
Kay Cee <8.kaycee@gmail.com>
2023-12-20 12:38:37 +0100

    feat: add auth schemas and controllers

--- server/app/core/config.js
+++ server/app/core/config.js
@@ -1,23 +1,27 @@
 require('dotenv').config()
 
 class Settings {
-  API_NAME = ''
-  API_HOST = ''
+  API_NAME = process.env.API_NAME || ''
+  API_HOST = process.env.API_HOST || ''
   API_PORT = process.env.API_PORT || 8008
   API_VERSION = ''
 
   CLIENT_URL = ''
 
-  ENVIRONMENT = process.env.NODE_ENV
+  ENVIRONMENT = process.env.NODE_ENV || ''
 
-  SENDGRID_API_KEY = ''
+  SENDGRID_API_KEY = process.env.SENDGRID_API_KEY || ''
 
   JWT_SECRET = process.env.JWT_SECRET || ''
   JWT_EXPIRATION = process.env.JWT_EXPIRATION || ''
 
-  DB_NAME = ''
-  DB_HOST = ''
-  DB_PORT = ''
+  DB_NAME = process.env.DB_NAME || ''
+  DB_HOST = process.env.DB_HOST || ''
+  DB_PORT = process.env.DB_PORT || ''
+
+  constructor () {
+    this.DB_URI = `mongodb://${this.DB_HOST}:${this.DB_PORT}/${this.DB_NAME}`
+  }
 }
 
 module.exports = new Settings()
---
+++ server/app/modules/auth/controllers/auth.js
@@ -0,0 +1,10 @@
+const UserModel = require('../../../models/user')
+const jwt = require('jsonwebtoken')
+
+module.exports = {
+  preSignup: function (req, res) {},
+
+  signup: function (req, res) {},
+
+  login: function (req, res) {}
+}
