a27cbc39579e177c9a53bd25bff57650da7cf6ca
Kelechi Egbuta <kelechi.k.egbuta@gmail.com>
2024-03-20 16:17:12 +0100

    chore: edit register component

--- src/features/auth/Register.js
+++ src/features/auth/Register.js
@@ -1,6 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Register = () => {
+  const [formData, setFormData] = useState({})
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate()
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() })
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage('Please fill out all fields.')
+    }
+    try {
+      setLoading(true)
+      setErrorMessage(null)
+      const res = await fetch('http://0.0.0.0:8008/api/auth/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      })
+      const data = await res.json()
+      if (data.success === false) {
+        return setErrorMessage(data.message)
+      }
+      setLoading(false)
+      if(res.ok) {
+        navigate('/login')
+      }
+    } catch (error) {
+      setErrorMessage(error.message)
+      setLoading(false)
+    }
+  }
+
   const btn = 'flex w-full items-center justify-center rounded-md py-1.5 text-sm'
 
   return (
@@ -19,34 +54,53 @@ const Register = () => {
           </div>
 
           <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
-            <form className='space-y-6' action='/register' method='POST'>
+            <form className='space-y-6' onSubmit={handleSubmit}>
               <div>
-                <label htmlFor='firstname' className='block text-sm font-medium leading-6 text-gray-900 dark:text-white'>
+                <label htmlFor='first_name' className='block text-sm font-medium leading-6 text-gray-900 dark:text-white'>
                   First name
                 </label>
                 <div className='mt-2'>
                   <input
-                    id='firstname'
+                    id='first_name'
                     name='firstname'
                     type='text'
                     autoComplete='text'
                     required
+                    onChange={handleChange}
                     className='block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6'
                   />
                 </div>
               </div>
 
               <div>
-                <label htmlFor='lastname' className='block text-sm font-medium leading-6 text-gray-900 dark:text-white'>
+                <label htmlFor='last_name' className='block text-sm font-medium leading-6 text-gray-900 dark:text-white'>
                   Last name
                 </label>
                 <div className='mt-2'>
                   <input
-                    id='lastname'
-                    name='llastname'
+                    id='last_name'
+                    name='lastname'
                     type='text'
                     autoComplete='text'
                     required
+                    onChange={handleChange}
+                    className='block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6'
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label htmlFor='username' className='block text-sm font-medium leading-6 text-gray-900 dark:text-white'>
+                  Username
+                </label>
+                <div className='mt-2'>
+                  <input
+                    id='username'
+                    name='username'
+                    type='text'
+                    autoComplete='text'
+                    required
+                    onChange={handleChange}
                     className='block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6'
                   />
                 </div>
@@ -63,6 +117,7 @@ const Register = () => {
                     type='email'
                     autoComplete='email'
                     required
+                    onChange={handleChange}
                     className='block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6'
                   />
                 </div>
@@ -81,6 +136,7 @@ const Register = () => {
                     type='password'
                     autoComplete='current-password'
                     required
+                    onChange={handleChange}
                     className='block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6'
                   />
                 </div>
@@ -89,6 +145,7 @@ const Register = () => {
               <div>
                 <button
                   type='submit'
+                  disabled={loading}
                   className={`${btn} bg-blue-600 py-2 px-3 rounded-lg font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600`}
                 >
                   Get started
@@ -118,17 +175,23 @@ const Register = () => {
                   <svg className='w-5 h-5' viewBox='0 0 48 48' fill='none' xmlns='http://www.w3.org/2000/svg'>
                     <path d='M15.095 43.5014C33.2083 43.5014 43.1155 28.4946 43.1155 15.4809C43.1155 15.0546 43.1155 14.6303 43.0867 14.2079C45.0141 12.8138 46.6778 11.0877 48 9.11033C46.2028 9.90713 44.2961 10.4294 42.3437 10.6598C44.3996 9.42915 45.9383 7.49333 46.6733 5.21273C44.7402 6.35994 42.6253 7.16838 40.4198 7.60313C38.935 6.02428 36.9712 4.97881 34.8324 4.6285C32.6935 4.27818 30.4988 4.64256 28.5879 5.66523C26.677 6.68791 25.1564 8.31187 24.2615 10.2858C23.3665 12.2598 23.1471 14.4737 23.6371 16.5849C19.7218 16.3885 15.8915 15.371 12.3949 13.5983C8.89831 11.8257 5.81353 9.33765 3.3408 6.29561C2.08146 8.4636 1.69574 11.0301 2.2622 13.4725C2.82865 15.9148 4.30468 18.0495 6.38976 19.4418C4.82246 19.3959 3.2893 18.9731 1.92 18.2092V18.334C1.92062 20.6077 2.7077 22.8112 4.14774 24.5707C5.58778 26.3303 7.59212 27.5375 9.8208 27.9878C8.37096 28.3832 6.84975 28.441 5.37408 28.1567C6.00363 30.1134 7.22886 31.8244 8.87848 33.0506C10.5281 34.2768 12.5197 34.9569 14.5747 34.9958C12.5329 36.6007 10.1946 37.7873 7.69375 38.4878C5.19287 39.1882 2.57843 39.3886 0 39.0777C4.50367 41.9677 9.74385 43.5007 15.095 43.4937' fill='#1DA1F2' />
                   </svg>
-                  Continue with Twitter
+                  Continue with X
                 </button>
               </div>
             </form>
 
             <p className='mt-10 text-center text-sm text-gray-500'>
               Already a member?{' '}
-              <a href='login' className='font-semibold leading-6 text-blue-600 hover:text-blue-500'>
+              <a href='/login' className='font-semibold leading-6 text-blue-600 hover:text-blue-500'>
                 Sign in
               </a>
             </p>
+
+            {errorMessage && (
+              <div className='rounded mt-5 bg-red-500'>
+                {errorMessage}
+              </div>
+            )}
           </div>
         </div>
       </section>
